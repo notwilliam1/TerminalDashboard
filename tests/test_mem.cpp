5634503d70ffe92ce8c63f7e243b3ad499b46760
William Causey <causeywilliam0@gmail.com>
2026-04-16 21:05:04 -0400

    Tests: added 3 test cases to test_mem to make sure memory usage does not exceed memory total and both are under 256GB. Choosing 256GB due to that generally being the most a consumer computer will have.

--- tests/test_mem.cpp
+++ tests/test_mem.cpp
@@ -10,3 +10,17 @@ TEST(GetTotalMemoryGB, ReturnsNonNegative) {
     float result = GetTotalMemoryGB();
     EXPECT_GE(result, 0);
 }
+
+TEST(GetMemoryUsageGB, UsedDoesNotExceedTotal) {
+    EXPECT_LE(GetMemoryUsageGB(), GetTotalMemoryGB());
+}
+
+TEST(GetTotalMemoryGB, TotalIsReasonable) {
+    float result = GetTotalMemoryGB();
+    EXPECT_LE(result, 256.0f); // max consumer memory size generally
+}
+
+TEST(GetMemoryUsageGB, UsageIsReasonable) {
+    float result = GetMemoryUsageGB();
+    EXPECT_LE(result, 256.0f);
+}
